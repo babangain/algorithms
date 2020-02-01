5293e5c9ecab0c6810c2908905357277f827f8eb
Baban Gain <gainbaban@gmail.com>
2020-02-01 19:09:23 +0530

    Update LCS.cpp

--- LCS.cpp
+++ LCS.cpp
@@ -1,5 +1,40 @@
 #include <bits/stdc++.h>
+#include <stack>
+
 using namespace std;
+
+void printLcs(vector<vector<int>> arr, string s1, string s2,int lcs_len)
+{
+    int l1 = s1.length(); // Orginal length + 1
+    int l2 = s2.length(); // Orginal length + 1
+    char res[lcs_len+1];
+    res[lcs_len] ='\0';
+    int i=l1-1, j=l2-1;
+    while (i>0 && j>0)
+    {
+        if(s1[i] == s2[j])
+        {
+            res[--lcs_len] = s1[i];
+            i--;
+            j--;
+        }
+        else
+        {
+            if(arr[i-1][j] > arr[i][j-1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+    }
+    cout<<res;
+    cout<<endl;
+
+
+}
 int lcs(string s1,string s2)
 {
     int l1 = s1.length();
@@ -23,14 +58,18 @@ int lcs(string s1,string s2)
         }
         //cout<<endl;
     }
-    return arr[l1][l2];
+    int lcs_len = arr[l1][l2];
+    printLcs(arr,s1,s2,lcs_len);
+    return lcs_len;
 }
+
 int32_t main()
 {
     string s1,s2;
     cin>>s1;
     cin>>s2;
     cout<<lcs(s1,s2);
+
 
 
 }
